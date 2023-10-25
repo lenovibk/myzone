3a10bf471fe775a1bdb41f2297a4b7011da803c0
lenovibk <75346111+lenovibk@users.noreply.github.com>
2023-10-25 18:19:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.use(
   })
 );
 
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
